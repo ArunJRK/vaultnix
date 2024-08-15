f779df156b78a88d19bdb0f54686dc7b764194ce
ArunJRK <arun.jrk99@gmail.com>
2024-08-15 16:13:48 +0530

    mTLS Test Successful

--- nginx-config/gen-conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create conf files for each Nginx server
-for i in {1..3}
-do
-    sed "s/SERVER_NAME/nginx$i.example.com/g" nginx-conf-template > nginx$i.conf
-done
-
-echo "Nginx configuration files created successfully!"
